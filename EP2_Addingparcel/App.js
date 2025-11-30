7ae44095470512d8a6302766cc04cd79da1311b7
12214614 <tejaelagonda@gmail.com>
2025-12-01 02:25:52 +0530

    Fix folder tracking

---
+++ EP2_Addingparcel/App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id: "heading"}, "Hello This is Day-2 of Namesthe React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
